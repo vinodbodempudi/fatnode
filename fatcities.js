fac2e8b48ed48024dbb082e37fd3b2a02472b548
arunreddyc <sunmyth.arun@gmail.com>
2014-06-27 07:26:42 -0500

    log4j and image
    configured log4j in fatproperties and fatcities and Code to save images
    in S3

--- fatcities.js
+++ fatcities.js
@@ -4,6 +4,10 @@ var Server = require('mongodb').Server;
 var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectId;
 
+var log4js = require( "log4js" );
+log4js.configure( "log4js.json" );
+var logger = log4js.getLogger( "test-file-appender" );
+
 FatCities = function(host, port){
 this.db = new Db('fatcities', new Server(host, port, {safe: false}, {auto_reconnect: true}, {}));
 this.db.open(function(){});
@@ -24,9 +28,12 @@ this.getCollection(function(error, cities_collection){
   if(error) callback(error)
   else{
     cities_collection.distinct('city', function(error, cities){
-	 if(error) callback(error)
+	 if(error) {
+	 	logger.debug(error);
+	 	callback(error);
+	 }
 	 else{
-     	callback(null, cities)
+     	callback(null, cities);
 	 }
 	});
 	}
