d1c932d4f46483194e2a79a754424cbe60642638
ramaguntu <rama.guntu@gmail.com>
2014-07-26 23:06:04 +0530

    added js files for ie cors issue

--- fatServer.js
+++ fatServer.js
@@ -43,8 +43,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb'}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/static', express.static(__dirname + '/public'));
 
 app.all('/*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
