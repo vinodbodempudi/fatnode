938448d0dfd80b51d96124736d71d5612790f3ad
ramaguntu <rama.guntu@gmail.com>
2014-10-04 14:54:35 +0530

    Filtered deleted props in results

--- fatProperties.js
+++ fatProperties.js
@@ -121,7 +121,7 @@ this.getCollection(function(error, properties_collection){
 	if(error) {
 		callback(error);
 	} else {
-		properties_collection.find({$or:[{"user._id":userId}, {"user.email":email}, {"user.primaryEmail":email}]},
+		properties_collection.find({'active': { $ne: 'D'}, $or:[{"user._id":userId}, {"user.email":email}, {"user.primaryEmail":email}]},
 			{"active":1, "details.mode":1 , "details.price.price":1, "details.monthlyRent":1, "createdDate":1, "lastUpdatedDate":1, "details.title":1, "location.lat":1, "location.lng":1
 			,"details.bedRooms":1, "details.bathRooms":1, "details.area.builtUp.builtUp":1, "details.area.builtUp.builtUpInSqft":1, "details.area.builtUp.units":1
 			,"details.area.perUnitPrice":1, "details.area.priceUnit":1, "user.locality":1, "details.propertySubType":1, "urls.coverPhotoUrl.url":1}).toArray(function(error, properties){
@@ -155,7 +155,7 @@ FatProperties.prototype.list = function(city, locality, callback){
 this.getCollection(function(error, properties_collection){
  if(error) callback(error);
   else{
-    properties_collection.find({"user.city":city, "user.locality":locality},
+    properties_collection.find({"user.city":city, "user.locality":locality, 'active': { $ne: 'D'}},
     	{"active":1, "details.mode":1 , "details.price.price":1, "details.monthlyRent":1, "createdDate":1, "lastUpdatedDate":1, "details.title":1, "location.lat":1, "location.lng":1
     	,"details.bedRooms":1, "details.bathRooms":1, "details.area.builtUp.builtUp":1, "details.area.builtUp.builtUpInSqft":1, "details.area.builtUp.units":1
     	,"details.area.perUnitPrice":1, "details.area.priceUnit":1, "user.locality":1, "details.propertySubType":1, "urls.coverPhotoUrl.url":1}).toArray(function(error, properties){
